7bda796b311de5c8ffe15504b856ce6844f0b18f
Zecharias Mengis <zmengis@gmail.com>
2021-02-08 22:10:41 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,4 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def get_user_input
   gets.chomp
@@ -20,5 +20,5 @@ end
 
 def runner
   prompt_user
-  selection(get_user_input)
+  selection(num)
 end
